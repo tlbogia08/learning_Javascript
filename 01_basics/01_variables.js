3620038830b14799b93c09b63e7cde54411a498e
Rudra <104748559+tlbogia08@users.noreply.github.com>
2025-08-12 08:47:56 +0000

    First commit after reinit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1229330
+let accountEmail = "rudra-jo-nhi-hai-@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gaya"
+let accountState;
+
+// accountId = 3;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); // values are undefined if not intialized
+
+/* Multi Line Comment
+
+    Prefer not use var
+    because issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") 
+// alert(3+3) we are using node js not browser so you can't run it 
+
+console.log("Hello"); 
+
+console.log(3+
+    3) // code readablity should be priority
+
+
+// string
+let name = "Rudra";
+let age = 20; // number
+let isLoggedIn = false; // false
+
+// number => 2^53
+// bigInt = specially in trading , rededit
+// string = " "
+// boolean = True or False
+// null  yeh ek standalone value hai (representation hai jagah abhi ke liye khali hai)
+// undefined ==> jab value defined or assign nhi kiya
+// symbol = unique values ko find karne ke liye
+
+// object
+/*
+null ek object hai
+wahi par undefined ek special type of datatype hi hai
+*/
