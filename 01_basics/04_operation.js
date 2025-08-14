d87151b8d2510ca23dbbab41d3359107e182284c
Rudra <104748559+tlbogia08@users.noreply.github.com>
2025-08-14 11:17:32 +0000

    Add conversion example in JavaScript

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,46 @@
+// ************************* Operations *******************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// Basic Operations
+    // console.log(2+2);
+    // console.log(2-2);
+    // console.log(2**2);
+    // console.log(2/2);
+    // console.log(2%2);
+    // console.log(2*2);
+
+let str1 = "Rudra";
+let str2 = " Sharma";
+
+let str3 = str1 + str2;
+// console.log(str3); // basic string operation
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// // just javascript thing
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3); // first modulo then (+)
+
+// console.log(-true + 1)
+// console.log(+"") // not prefred
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 4
+
+let gameCounter = 100
+gameCounter++; // postfix (standalone work similar)
+++gameCounter; // prefix (depended on when paired)
+console.log(gameCounter);
+
+// More on Type Conversion
+
+// link: https://tc39.es/ecma262/multipage/abstract-operations.html
+
+// And more on postfix & prefix through mdn
+
+
+
---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 <= 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/*
+    ****************This type of conversion can make us confused any time be aware of this type of conversion **********
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined  == 0);
+console.log(undefined  >= 0);
+console.log(undefined  <= 0);
+console.log(undefined  < 0);
+console.log(undefined  > 0);
+*/
+// strict check
+console.log("2" === 2); // false because it checks data type too
+
